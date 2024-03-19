df90f7e127afd447e5e0523bcd8db2a4daace6fe
Elaidzha1940 <eli080293@gmail.com>
2024-03-19 18:32:10 +0300

    Update ViewController

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,9 +11,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji)")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = .white
+        }
     }
 }
 
